b51cbd89c252d3ff8661ff9f0388ef90a14502b7
Peter Coker <petercoker@protonmail.com>
2025-06-14 07:56:33 +0200

    completed from  exercise level 1  from 1 to 14

--- Exercises/2_Day/hello.js
+++ Exercises/2_Day/hello.js
@@ -1,1 +1,27 @@
-console.log("2 days");
+let challenge = "30 Days of JavaScript";
+
+console.log(challenge);
+
+console.log(challenge.length);
+
+console.log(challenge.toUpperCase());
+
+console.log(challenge.toLowerCase());
+
+console.log(challenge.substring(0, 2));
+
+console.log(challenge.substring(2));
+
+console.log(challenge.includes("Script"));
+
+console.log(challenge.split(" "));
+
+let company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+console.log(company.split(" "));
+
+console.log(challenge.replace("JavaScript", "Pyton"));
+
+console.log(challenge.charAt(15));
+
+console.log(challenge.charCodeAt("J"));

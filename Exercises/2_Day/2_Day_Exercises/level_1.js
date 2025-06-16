176f7b034484b97173a2304b79bf103cd927fbe6
Peter Coker <petercoker@protonmail.com>
2025-06-16 07:46:55 +0200

    complete day 2, level 1 exercise 17 and 18

--- Exercises/2_Day/2_Day_Exercises/level_1.js
+++ Exercises/2_Day/2_Day_Exercises/level_1.js
@@ -30,4 +30,28 @@ console.log(challenge.indexOf("a"));
 
 console.log("Here");
 
-console.log(challenge.lastIndexOf("a")); //SECTION : level 16
+console.log(challenge.lastIndexOf("a"));
+
+let sentence =
+  "You cannot end a sentence with because because because is a conjunction";
+const searchTerm = "because";
+const indexOfFirst = sentence.indexOf(searchTerm);
+
+console.log(
+  `The index of the first occurrence "${searchTerm}" is ${indexOfFirst}`
+);
+
+console.log(
+  `The index of the second occurrence  "${searchTerm}" is ${sentence.indexOf(
+    searchTerm,
+    indexOfFirst + 1
+  )}`
+);
+
+console.log(
+  `The index of the last occurrence "${searchTerm}" is ${sentence.lastIndexOf(
+    searchTerm
+  )}`
+);
+
+//SECTION : next level 19

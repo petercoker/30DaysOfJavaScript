457da560d993925a2b26bead53f14ba3c14856a6
Peter Coker <petercoker@protonmail.com>
2025-06-27 08:09:05 +0200

    complete day 2, level 3 exercise 3

--- Exercises/2_Day/level_3.js
+++ Exercises/2_Day/level_3.js
@@ -6,6 +6,9 @@ const sentenceAboutLove =
 
 const countWord = "love";
 
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+
 console.log(sentenceWithConjunction.substring(31, 54));
 
 const sentenceAboutLoveToLowerCase = sentenceAboutLove.toLowerCase();
@@ -16,3 +19,6 @@ console.log(
 
 // SECTION level 3 exercise 1
 console.log(sentenceWithConjunction.match(/because/g).length);
+
+// Removing all characters except letters and numbers
+console.log(sentence.replace(/[^\p{L}\d\s]+/gu, ""));

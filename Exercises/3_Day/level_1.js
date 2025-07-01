89ad34fd4456eaf127610bc32ae10319a161e1f1
Peter Coker <petercoker@protonmail.com>
2025-07-01 23:19:32 +0200

    regular expression basic part 2

--- Exercises/3_Day/level_1.js
+++ Exercises/3_Day/level_1.js
@@ -2,4 +2,4 @@
 let data =
   "The fat cat ran down the street. It was searching for a mouse to ear.";
 // console.log(data.match(/fat/g));
-console.log(data.match(/The/g));
+console.log(data.match(/e+/g));

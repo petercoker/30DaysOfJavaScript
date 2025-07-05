685b42cf3142896a2b340c7608ec79a29c3c9c18
Peter Coker <petercoker@protonmail.com>
2025-07-05 18:21:16 +0200

    learn meta character

--- Exercises/3_Day/level_1.js
+++ Exercises/3_Day/level_1.js
@@ -2,4 +2,7 @@
 let data =
   "The fat cat ran down the street. It was searching for a mouse to ear.";
 // console.log(data.match(/fat/g));
-console.log(data.match(/e+/g));
+// console.log(data.match(/the/gi));
+
+data = "The car parked in the garage.";
+console.log(data.match(/.ar/g)); // . Period matches any single character expect a line break

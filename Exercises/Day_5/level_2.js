a0e2d8e233abcddb5933d3543d2d41c674153053
Peter Coker <petercoker@protonmail.com>
2026-01-03 09:27:40 +0100

    completed day 5 level 2 ex 6

--- Exercises/Day_5/level_2.js
+++ Exercises/Day_5/level_2.js
@@ -131,3 +131,11 @@ if (exist) {
   const updatedWebTechs = [...webTechs, "Sass"];
   console.log(updatedWebTechs);
 }
+
+// 6. Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+
+const fullStack = [...frontEnd, ...backEnd];
+console.log(fullStack);

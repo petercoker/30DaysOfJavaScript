8348d4856f1fbd0ccb87e3d493d40114c3d090dc
Peter Coker <petercoker@protonmail.com>
2026-01-05 08:43:58 +0100

    refactor loops in Day 6 level 1 , 2, 3, 4 exercise for clarity and consistency

--- Exercises/Day_6/level_1.js
+++ Exercises/Day_6/level_1.js
@@ -24,27 +24,90 @@ const webTechs = [
 
 const mernStack = ["MongoDB", "Express", "React", "Node"];
 
-const limit = 10;
+let limit = 10;
 
 // 1, Iterate 0 to 10 using for loop, do the same using while and do while loop
-// for (let index = 0; index < limit; index++) {
-//   console.log(countries[index]);
-// }
 
-// const index = 0;
-// while (index <= limit) {
-//   console.log(countries[index]);
-//   index++;
-// }
-
-let i = 0;
-do {
+for (let i = 0; i < limit; i++) {
   console.log(countries[i]);
-  i++
-} while (i <= limit);
+}
 
-let index = 0
+let w = 0;
+while (w < limit) {
+  console.log(countries[w]);
+  w++;
+}
+
+let d = 0;
 do {
-  console.log(countries[index]);
-  index++
-} while (index <= limit)
+  console.log(countries[d]);
+  d++;
+} while (d <= limit);
+
+// 2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+for (let i = limit; i >= 0; i--) {
+  console.log(countries[i]);
+}
+
+w = limit;
+while (w >= 0) {
+  console.log(countries[w]);
+  w--;
+}
+
+d = limit;
+do {
+  console.log(countries[d]);
+  d--;
+} while (d >= 0);
+
+// 3. Iterate 0 to n using for loop
+for (let i = 0; i < countries.length; i++) {
+  console.log(countries[i]);
+}
+
+w = 0;
+while (w < countries.length) {
+  console.log(countries[w]);
+  w++;
+}
+
+d = 0;
+do {
+  console.log(countries[d]);
+  d++;
+} while (d <= limit);
+
+// Write a loop that makes the following pattern using console.log():
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+let n = 7;
+for (let i = 1; i <= n; i++) {
+  let str = "# ";
+  console.log(str.repeat(i));
+}
+
+// let rows = 7;
+
+// // pattern variable carries the final pattern in string format
+// let pattern = "#";
+
+// // outer loop runs for `rows` no. of times
+// for (let n = 1; n <= rows; n++) {
+//   console.log(pattern);
+
+//   // inner loop runs for n
+//   for (let num = 1; num <= n; num++) {
+//     console.log(pattern);
+//   }
+
+//   // Add a new line character after contents of each line
+//   pattern += "\n";
+// }
+// // console.log(pattern);
